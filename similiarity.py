0bc9486ad668f2457b9a480ad70d5f9bf1871803
wwvmd <vm.codemonkey@yahoo.com>
2026-09-21 07:53:14 +0100

    progress

--- similiarity.py
+++ similiarity.py
@@ -23,11 +23,29 @@ with open("products.json") as f:
     products = json.load(f)
 
 
-list_of_descriptions = ['Charge your devices conveniently with this sleek wireless charging dock.', 'Elevate your skincare routine with this luxurious skincare set.']
+
+# list_of_descriptions = ['Charge your devices conveniently with this sleek wireless charging dock.', 'Elevate your skincare routine with this luxurious skincare set.']
 
 
-# Embed short_description and print
-print(create_embeddings(products[0]['short_description'])[0])
+# # Embed short_description and print
+# print(create_embeddings(products[0]['short_description'])[0])
 
-# Embed list_of_descriptions and print
-print(create_embeddings(list_of_descriptions))
+# # Embed list_of_descriptions and print
+# print(create_embeddings(list_of_descriptions))
+
+product_to_find = "soap"
+product_to_find_embedding = create_embeddings(product_to_find)[0]
+
+
+for product in products:
+    product['embedding'] = create_embeddings(product['short_description'])[0]
+
+distances = []
+
+for product in products:
+    row = []
+    row.append(distance.cosine(product['embedding'], product_to_find_embedding))
+    distances.append(row)
+
+import numpy as np
+print(f"closest product to {product_to_find}:  {products[np.argmin([row[0] for row in distances])]['short_description']}");
